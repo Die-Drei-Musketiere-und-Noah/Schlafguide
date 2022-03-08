be0b6d64e49beeea9223cf37071702b41bfe66d6
DE\BOD6SGM <Damian.BOck@bosch.com>
2022-03-08 10:50:44 +0100

    Content fehlt und mobile Version

--- src/subpages/Sleepconditions/Sleepconditions.js
+++ src/subpages/Sleepconditions/Sleepconditions.js
@@ -1,5 +1,5 @@
 const TexteInfo = ["Fenster","Lampe","Laptop","Kissen","Pflanze","Wand","Decke","Boden","Schrank","Matratze","Bettdecke","Bett","Tisch","Fenster Es wird eine Zimmertemperatur von etwa 16°C-21°C bei Erwachsenen empfohlen wobei eine kältere Temperatur weniger dem Schlaf schadet als eine höhere.\n Grund dafür ist, der zirkadiane Rhythmus des Menschen der am Abend die Körpertemperatur und von einer unidealen Temperatur beinflusst werden kann.","1","2","3","Zwar führt bessere Luft Qualität zu einem verbesserten Schlaf, dennoch können Pflanzen diese nicht erzeugen, weshalb man in der Nacht sein Fenster offen lassen muss um eine gute Luftqualität zu erreichen.","5","6","7","8","9","10","11","12","13"]
-
+const TexteInfo2 = ["Musik","Hantel","Handy","Uhr","Badewanne","Schuh","Thermometer","Miclh","1","2","3","4","5","6","7","8"]
 
 function changeinfoText(i)
 {   
@@ -8,21 +8,11 @@ function changeinfoText(i)
         el.textContent = TexteInfo[i];
         el2.textContent =TexteInfo[i+13];
 }
-
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function changeinfoText2(i)
+{   
+    var el = document.getElementById("Info3");
+    var el2 = document.getElementById("Info4");
+        el.textContent = TexteInfo[i];
+        el2.textContent =TexteInfo[i+8];
 }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("imageslider");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
-}
