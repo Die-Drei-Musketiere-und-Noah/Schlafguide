3466eebadebb6a26b763dd437f455f4e18cce2c5
Snerksss <simon.berndt@t-online.de>
2022-03-28 16:08:43 +0200

    update of creating Chart via GPS Cords

--- src/subpages/dataMata/worldData.js
+++ src/subpages/dataMata/worldData.js
@@ -1,31 +1,70 @@
-let ctx = document.getElementById("myChart");
+
 
 let days = [];
 let moonphase = [];
 
-
-let display;
+let lat = 48.6818466;
+let long = 10.15458;
 
 let myChart;
 
-$.getJSON('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Heidenheim?unitGroup=metric&elements=sunrise%2Csunset%2Cmoonphase&include=fcst%2Cdays&key=WCHSBS53WTNRQ3WHQ8CNM74DQ&contentType=json'
-    , function (data) {
-        for (let date in data.days) {
-            days.push(date);
-            moonphase.push(data.days[date].moonphase)
-        }
-        myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: days,
-                datasets: [
-                    {
-                        backgroundColor: 'rgb(0,0,255)',
-                        label: 'Sicht auf Mond',
-                        data: moonphase
-                    }
-                ]
-            }
-        });
 
-    });
+function getGeolocation(){
+    if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(startCalculateChart, showError);
+    }
+}
+
+function startCalculateChart(position) {
+    lat = position.coords.latitude;
+    long = position.coords.longitude;
+    document.getElementById("cityName").innerHTML = lat + ',' + long;
+    chart();
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            document.getElementById("cityName").innerHTML = 'Sie haben die Ortung verboten. <br> Koordinaten der DHBW'
+            chart();
+            break;
+        case error.POSITION_UNAVAILABLE:
+            document.getElementById("cityName").innerHTML = 'Die Ortung steht nicht zur Verfügung. <br> Koordinaten der DHBW'
+            chart();
+            break;
+        case error.TIMEOUT:
+            document.getElementById("cityName").innerHTML = 'Timeout bei der Ortung. <br> Koordinaten der DHBW'
+            chart();
+            break;
+        case error.UNKNOWN_ERROR:
+            document.getElementById("cityName").innerHTML = 'Unbekannter Fehler bei der Ortung. <br> Koordinaten der DHBW'
+            chart();
+            break;
+    }
+}
+
+function chart() {
+    $.getJSON('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + lat + ',' + long + '?unitGroup=metric&elements=sunrise%2Csunset%2Cmoonphase&include=fcst%2Cdays&key=WCHSBS53WTNRQ3WHQ8CNM74DQ&contentType=json'
+        , function (data) {
+            let counter = 0;
+            for (let dateNr in data.days) {
+                days.push(dateNr);
+                moonphase.push(data.days[dateNr].moonphase);
+            }
+            let ctx = document.getElementById("myChart");
+            myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: days,
+                    datasets: [
+                        {
+                            backgroundColor: 'rgb(0,0,255)',
+                            label: 'Sicht auf Mond',
+                            data: moonphase
+                        }
+                    ]
+                }
+            });
+
+        });
+}
